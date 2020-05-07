3eae696a044e735c9f56ca5d061874a41b694152
mmerts <mmert.suerkan@tedu.edu.tr>
2020-05-07 22:40:25 +0300

    LeadTheWay1.0.1 -RegisterCompleted

--- app/src/main/java/com/example/leadtheway/MainActivity.java
+++ app/src/main/java/com/example/leadtheway/MainActivity.java
@@ -10,33 +10,33 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonsecondpage;
-
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         buttonsecondpage = (Button) findViewById(R.id.buttonmain);
+        registerButton = (Button) findViewById(R.id.buttonregister);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
         buttonsecondpage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OpenSecondPage();
+                Intent intent = new Intent(MainActivity.this, SecondpageActivity.class);
+                startActivity(intent);
 
             }
 
         });
 
     }
-
-
-
-
-    public void OpenSecondPage(){
-
-        Intent intent = new Intent(this, SecondpageActivity.class);
-        startActivity(intent);
-    }
-
 
 
 }

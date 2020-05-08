4e8c261e0923794c3ce5e0d9c60122041cc169a8
mmerts <mmert.suerkan@tedu.edu.tr>
2020-05-08 14:58:16 +0300

    Test

--- app/src/main/java/com/example/leadtheway/MainActivity.java
+++ app/src/main/java/com/example/leadtheway/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //test
         buttonsecondpage = (Button) findViewById(R.id.buttonmain);
         registerButton = (Button) findViewById(R.id.buttonregister);
 

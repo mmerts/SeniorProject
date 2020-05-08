40b75e5828af0bcddc3b5a06721dbc18c8e81d9d
mmerts <mmert.suerkan@tedu.edu.tr>
2020-05-08 15:59:35 +0300

    Test

--- app/src/main/java/com/example/leadtheway/MainActivity.java
+++ app/src/main/java/com/example/leadtheway/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonsecondpage;
     private Button registerButton;
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
